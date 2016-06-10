01339563436e471619acfee48c5d73900cccd925
Flancy <flancyk.flancyk@gmail.com>
2016-06-10 12:48:10 +0300

    Первый коммит

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль должен содержать минимум 6 символов и совпадать.',
+    'reset' => 'Ваш пароль восстановлен!',
+    'sent' => 'Мы отправили Вам на e-mail ссылку для восстановления пароля!',
+    'token' => 'Этот пароль не подходит.',
+    'user' => "Нет пользователя с таким e-mail.",
+
+];
